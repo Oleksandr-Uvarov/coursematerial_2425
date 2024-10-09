2dae65a975fa96e976d1f052f414d3f1be726af8
Oleksandr-Uvarov <oleksandr.uvarov@student.ucll.be>
2024-10-09 11:45:31 +0200

    Higher card 2nd solution + 4 conditional tasks solved.

--- 02-booleans/07-assignment-earlier/student.py
+++ 02-booleans/07-assignment-earlier/student.py
@@ -1,1 +1,12 @@
 # write your code here
+def earlier(h1, m1, h2, m2):
+    # if h1 < h2:
+    #         return True
+    # elif h1 == h2:
+    #       if m1 < m2:
+    #             return True
+
+    # return False
+    if (h1 * 60 + m1 < h2 * 60 + m2):
+        return True
+    return False
--- 02-booleans/08-assignment-higher-card/student.py
+++ 02-booleans/08-assignment-higher-card/student.py
@@ -1,1 +1,9 @@
 # write your code here
+def higher_card(card1, card2):
+    if card2 == 1:
+        return False
+    elif card1 == 1:
+        return True
+    elif card1 > card2:
+        return True
+    return False
