019de7c561de153be3da5766a59809d299518ae6
Oleksandr-Uvarov <oleksandr.uvarov@student.ucll.be>
2024-10-09 11:45:31 +0200

    Boolean exercises sovled.

--- 02-booleans/06-assignment-dates/student.py
+++ 02-booleans/06-assignment-dates/student.py
@@ -1,7 +1,62 @@
 # write your code here
 def is_valid_month(month):
- if month >= 1 and month <= 12:
-  return True
- 
+	if month >= 1 and month <= 12:
+		return True
+	return False
+
+
 def is_leap_year(year):
- return None
+	if year % 400 == 0:
+		return True
+	elif year % 100 == 0:
+		return False
+	elif year % 4 == 0:
+		return True
+	return False
+
+
+def has_30_days(month):
+	if is_valid_month(month):
+		if month == 4 or month == 6 or month == 9 or month == 11:
+			return True
+	return False
+
+
+def has_28_days(month, year):
+	if is_leap_year(year):
+		return False
+	else:
+		if month == 2:
+			return True
+	return False
+
+
+def has_31_days(month):
+	if is_valid_month(month):
+		if has_30_days(month) == False:
+			if month != 2:
+				return True
+	return False
+
+def has_29_days(month, year):
+	if is_leap_year(year):
+		if month == 2:
+			return True
+	return False
+
+
+def is_valid_date(day, month, year):
+	if is_valid_month(month):
+		if has_30_days(month):
+			if 1 <= day <= 30:
+				return True
+		elif has_31_days(month):
+			if 1 <= day <= 31:
+				return True
+		elif has_28_days(month, year):
+			if 1 <= day <= 28:
+				return True
+		elif has_29_days(month, year):
+			if 1 <= day <= 29:
+				return True
+	return False
