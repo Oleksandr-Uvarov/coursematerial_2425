457bff08d5e612135db8cc9f0a5f8994ae50c745
Oleksandr-Uvarov <oleksandr.uvarov@student.ucll.be>
2024-10-09 22:41:29 +0200

    Conditionals finished.

--- 03-conditionals/08-assignment-player-movement/student.py
+++ 03-conditionals/08-assignment-player-movement/student.py
@@ -1,1 +1,11 @@
 # write your code here
+def player_movement(position, left_arrow, right_arrow, shift):
+    position_change = 1
+    if shift:
+        position_change = 2
+
+    if left_arrow:
+        position -= position_change
+    if right_arrow:
+        position += position_change
+    return position
--- 03-conditionals/09-assignment-movie-ticket/student.py
+++ 03-conditionals/09-assignment-movie-ticket/student.py
@@ -1,1 +1,20 @@
 # write your code here
+def movie_ticket(duration, imax, student, ticket_count):
+    cost = 0
+
+    if duration < 90:
+        cost = 10
+    elif 90 <= duration < 120:
+        cost = 11
+    elif 120 <= duration < 150:
+        cost = 12
+    else:
+        cost = 15
+
+    if imax:
+        cost *= 1.2
+
+    if student:
+        cost -= 3
+
+    return cost * ticket_count
