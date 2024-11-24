26fde035bd99df00bc1a7a90064a4665e8d8debe
Oleksandr-Uvarov <oleksandr.uvarov@student.ucll.be>
2024-11-24 15:38:09 +0100

    Interval done.

--- 12-objects/07-assignment-interval/student.py
+++ 12-objects/07-assignment-interval/student.py
@@ -4,17 +4,28 @@ class Interval:
         self.upper = upper
 
 
-        def is_empty(self):
-            if lower > upper:
-                return True
-            return False
+    def is_empty(self):
+        if self.lower > self.upper:
+            return True
+        return False
         
 
-        def contains(self, number):
-            return number >= lower and number <= upper
+    def contains(self, number):
+        return number >= self.lower and number <= self.upper
 
         
 
-        def overlaps_with(self, other_interval):
-           return other_interval.contains(self.lower) or other_interval.contains(self.upper)
+    def overlaps_with(self, other_interval):
+        if not (self.is_empty() or other_interval.is_empty()):
+            return (other_interval.contains(self.lower)
+                     or other_interval.contains(self.upper)) or (self.contains(other_interval.lower
+                                                                                or self.contains(other_interval.upper)))
+        
+        return False
+
+
+
+interval_1 = Interval(1, 4)
+interval_2 = Interval(4, 8)
+print(interval_1.overlaps_with(interval_2))
             
